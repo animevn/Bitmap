f2cae82e5ff77f30e1b239cd974fc4e857eda2f7
animevn <haanhth21@gmail.com>
2020-03-24 17:52:03 +0700

    change git name

--- app/src/main/java/com/haanhgs/app/bitmapdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/bitmapdemo/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Display;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+//
 public class MainActivity extends AppCompatActivity {
 
     private final Point point = new Point();
